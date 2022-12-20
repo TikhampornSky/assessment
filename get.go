e69a45da128868a1834988c8905ffc06e22d7586
TikhampornSky <tikhamporntan@gmail.com>
2022-12-20 20:08:42 +0700

    Complete GET by ID

--- get.go
+++ get.go
@@ -1,11 +1,64 @@
 package main
 
 import (
+	"database/sql"
+	"database/sql/driver"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
+	"github.com/lib/pq"
 )
 
-func getExpenseHandler(c echo.Context) error {
+type Tags []string
+
+func (t Tags) ConvertArray() (driver.Value, error) {
+    if len(t) == 0 {
+        return "[]", nil
+    }
+    return fmt.Sprintf(`["%s"]`, strings.Join(t, `","`)), nil
+}
+
+func getExpensesHandler(c echo.Context) error {
+	stmt, err := db.Prepare("SELECT id, title, amount, note, tags FROM expenses")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't prepare query all expense statment:" + err.Error()})
+	}
+
+	rows, err := stmt.Query()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't query all expense:" + err.Error()})
+	}
+
+	expenses := []Expense{}
+	for rows.Next() {
+		e := Expense{}
+		err := rows.Scan(&e.ID, &e.Title, &e.Amount, &e.Note, pq.Array(&e.Tags))
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, Err{Message: "can't scan expense:" + err.Error()})
+		}
+		expenses = append(expenses, e)
+	}
 	return c.JSON(http.StatusOK, expenses)
+}
+
+func getExpenseHandler(c echo.Context) error {
+	id := c.Param("id")
+	stmt, err := db.Prepare("SELECT id, title, amount, note, tags FROM expenses WHERE id = $1")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't prepare query expense statment:" + err.Error()})
+	}
+
+	row := stmt.QueryRow(id)
+	e := Expense{}
+	err = row.Scan(&e.ID, &e.Title, &e.Amount, &e.Note, pq.Array(&e.Tags))
+	switch err {
+	case sql.ErrNoRows:
+		return c.JSON(http.StatusNotFound, Err{Message: "expense not found"})
+	case nil:
+		return c.JSON(http.StatusOK, e)
+	default:
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't scan expense:" + err.Error()})
+	}
 }
--- server.go
+++ server.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
-	"database/sql"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -19,12 +19,12 @@ type Err struct {
 type Expense struct {
 	ID     int      `json:"id"`
 	Title  string   `json:"title"`
-	Amount int      `json:"amount"` //transfer Name --> name (ตอนรับข้อมูล)
+	Amount float64  `json:"amount"` //transfer Name --> name (ตอนรับข้อมูล)
 	Note   string   `json:"note"`
 	Tags   []string `json:"tags"`
 }
 
-var expenses = []Expense{
+var expenses_tmp = []Expense{
 	{
 		ID:     1,
 		Title:  "strawberry smoothie",
@@ -51,7 +51,7 @@ func InitDB() {
 		note TEXT,
 		tags TEXT[]
 	);`
-	
+
 	_, err = db.Exec(createTb)
 
 	if err != nil {
@@ -67,7 +67,8 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/expenses", getExpenseHandler)
+	e.GET("/expenses", getExpensesHandler)
+	e.GET("/expenses/:id", getExpenseHandler)
 	e.POST("/expenses", createExpenseHandler)
 
 	fmt.Println("Please use server.go for main file")
