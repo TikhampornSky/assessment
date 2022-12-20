5a73c301029e36da12bd6f2937660be5ace2f48f
TikhampornSky <tikhamporntan@gmail.com>
2022-12-20 14:45:59 +0700

    Use Echo

--- server.go
+++ server.go
@@ -3,9 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"log"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
+	log.Fatal(e.Start(":2565"))
+	log.Println("Exist! Byr bye")
 }
