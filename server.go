03a4aabd729a5af6b1221352b32e24549bc9e5c6
TikhampornSky <tikhamporntan@gmail.com>
2022-12-21 11:38:29 +0700

    Gracful Shutdown

--- server.go
+++ server.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/TikhampornSky/assessment/repos"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
 
 	_ "github.com/lib/pq"
 )
@@ -37,6 +40,7 @@ func main() {
 	repos.InitDB()
 
 	e := echo.New()
+	e.Logger.SetLevel(log.INFO)
 
 	// e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
 	// 	if username == "November 10, 2009wrong_token" {
@@ -55,6 +59,19 @@ func main() {
 
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
-	log.Fatal(e.Start(":2565"))
-	log.Println("Exist! Byr bye")
+	go func() {
+		if err := e.Start(":2565"); err != nil && err != http.ErrServerClosed { // Start server
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt)
+	<-shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+	fmt.Println("Exist! Byr bye")
 }
